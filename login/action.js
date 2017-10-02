5b0bc406d4ecf0078d83bcbc00e2fce262de955f
Zac Hadjineophytou <zac1999ad@gmail.com>
2017-10-02 20:58:17 +0100

    Login updated
    Layout updated. Using action?mode=resetPassword will now call a function (nothing will happen yet)

---
+++ login/action.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var mode = getParameterByName('mode');
+
+
+  switch (mode) {
+    case 'resetPassword':
+      handleResetPassword();
+      break;
+    case 'recoverEmail':
+      handleRecoverEmail();
+      break;
+    default:
+  }
+}, false);
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+    var regexS = "[\\?&]"+name+"=([^&#]*)";
+    var regex = new RegExp(regexS);
+    var results = regex.exec(window.location.href);
+
+    if(results == null)
+        return "";
+    else
+        return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function handleResetPassword(auth, actionCode) {
+  var accountEmail;
+  // Verify the password reset code is valid.
+  auth.verifyPasswordResetCode(actionCode).then(function(email) {
+    var accountEmail = email;
+
+    // TODO: Show the reset screen with the user's email and ask the user for
+    // the new password.
+
+    // Save the new password.
+    auth.confirmPasswordReset(actionCode, newPassword).then(function(resp) {
+      // Password reset has been confirmed and new password updated.
+
+      // TODO: Display a link back to the app, or sign-in the user directly
+      // if the page belongs to the same domain as the app:
+      // auth.signInWithEmailAndPassword(accountEmail, newPassword);
+    }).catch(function(error) {
+      // Error occurred during confirmation. The code might have expired or the
+      // password is too weak.
+    });
+  }).catch(function(error) {
+    // Invalid or expired action code. Ask user to try to reset the password
+    // again.
+  });
+}
